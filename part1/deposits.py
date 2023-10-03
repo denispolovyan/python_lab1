395639c1c3f3038a51893f9f987266ac84e495ce
denispolovyan <denispolovyan@gmail.com>
2023-10-03 20:01:30 +0300

    feature: done todo

--- part1/deposits.py
+++ part1/deposits.py
@@ -12,10 +12,14 @@ def deposit(initial_sum, percent, fixed_period, set_period):
     """Calculate deposit yield."""
     per = percent / 100
     growth = (1 + per) ** (set_period / fixed_period)
-    
+    growthPerYear = (1 + per) ** (1 / fixed_period)
+    growthPerTenYear = (1 + per) ** (10 / fixed_period)
+
     # new feature: round result
-    
-    return round(float(initial_sum * growth), 2)
+
+    return [[round(float(initial_sum * growth), 2), round(growth*10, 2)],
+            [round(float(initial_sum * growthPerYear), 2), round(growthPerYear*10, 2)],
+            [round(float(initial_sum * growthPerTenYear), 2), round(growthPerTenYear*10, 2)]]
 
 
 def main(args):
@@ -30,25 +34,24 @@ def main(args):
     for i in range(4):
 
         if not args[i].isdigit() or int(args[i]) < 1:
-           
+
             try:
                 if float(args[i]) < 1 or float(args[i]):
-                   raise ValueError()
-                
+                    raise ValueError()
+
             except ValueError:
                 print('ARGS ERROR: arg (', args[i], ') is NOT a correct.')
                 exit()
-                
-                
 
     initial_sum, percent, fixed_period, set_period = map(float, args)
     res = deposit(initial_sum, percent, fixed_period, set_period)
-    print(res)
-    
-    
+
+    print('Your deposit:', res[0][0], ' Percents up:', res[0][1],'%')
+    print('Your deposit 1 year later:', res[1][0], ' Percents up:', res[1][1],'%')
+    print('Your deposit 10 years later:', res[2][0], ' Percents up:', res[2][1],'%')
+
 
 # start ========================>
-
 if __name__ == '__main__':
     import sys
 
