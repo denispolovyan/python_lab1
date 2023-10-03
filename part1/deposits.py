c79c5421837a024a7a440bfdb3fc1abe93c79a78
denispolovyan <denispolovyan@gmail.com>
2023-10-03 16:00:09 +0300

    feature: rounded sum & added check for incorrect args value

---
+++ part1/deposits.py
@@ -0,0 +1,55 @@
+# usage message ================>
+
+USAGE = """USAGE: {script} initial_sum percent fixed_period set_period
+
+\tCalculate deposit yield. See script source for more details.
+"""
+
+USAGE = USAGE.strip()
+
+
+def deposit(initial_sum, percent, fixed_period, set_period):
+    """Calculate deposit yield."""
+    per = percent / 100
+    growth = (1 + per) ** (set_period / fixed_period)
+    
+    # new feature: round result
+    
+    return round(float(initial_sum * growth), 2)
+
+
+def main(args):
+    """Gets called when run as a script."""
+    if len(args) != 4 + 1:
+        exit(USAGE.format(script=args[0]))
+
+    args = args[1:]
+
+    # new feature: check for corrent number
+
+    for i in range(4):
+
+        if not args[i].isdigit() or int(args[i]) < 1:
+           
+            try:
+                if float(args[i]) < 1 or float(args[i]):
+                   raise ValueError()
+                
+            except ValueError:
+                print('ARGS ERROR: arg (', args[i], ') is NOT a correct.')
+                exit()
+                
+                
+
+    initial_sum, percent, fixed_period, set_period = map(float, args)
+    res = deposit(initial_sum, percent, fixed_period, set_period)
+    print(res)
+    
+    
+
+# start ========================>
+
+if __name__ == '__main__':
+    import sys
+
+    main(sys.argv)
